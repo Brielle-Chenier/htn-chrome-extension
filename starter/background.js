bb0349c34ee6aa29711085163fc3948fa2367af0
Brielle-Chenier <brielle_chenier@live.com>
2021-01-14 22:19:44 -0500

    Update background.js

--- starter/background.js
+++ starter/background.js
@@ -1,15 +1,15 @@
+
 var pageConditions = {
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
-    pageUrl: {schemes: ["https", "http"]}
+      pageUrl: { schemes: ['https','http'] }
     })
   ],
   actions: [new chrome.declarativeContent.ShowPageAction()]
 }
 
-// Background.js
-chrome.runtime.onInstalled.addListener(function(){
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([pageConditions]);
   });
 });
